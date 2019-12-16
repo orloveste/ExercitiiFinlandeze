acb0b944c5a8d94d18d44e1c11556e564771f5c3
orloveste <orlovevm@yahoo.com>
2019-12-16 08:43:41 +0200

    1.4 Components of commands 1.4.1 Semicolon A semicolon ; is used to separate different commands. The compiler and the interpreter both ignore line breaks in the source code, so we could write the entire program on a single line. In the example below we will use the System.out.print command, which is similar to the System.out.println command except that it will not print a line break after printing the text. Example of how the semicolons are used System.out.print("Hello "); System.out.print("world"); System.out.print("!");
    Hello world!
    
    Even though neither the compiler nor the interpreter need line breaks in the source code, they are very important when considering human readers of the source code. Line breaks are required to divide source code in a clear manner. Readability of source code will be emphasized throughout this course.

--- src/week_1/The_program_and_the_source_code.java
+++ src/week_1/The_program_and_the_source_code.java
@@ -2,6 +2,7 @@ package week_1;
 
 public class The_program_and_the_source_code {
 public static void main(String[]args){
-    System.out.println("Hello world");
+    System.out.print("Hello ");System.out.print("world");System.out.print("!");
+
         }
 }
