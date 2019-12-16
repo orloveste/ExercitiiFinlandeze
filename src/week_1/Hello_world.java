524d7383754d72a9bde8f0730480c942a64ea460
orloveste <orlovevm@yahoo.com>
2019-12-16 09:36:36 +0200

    Exercise 2: Hello world! (And all the people of the world)

---
+++ src/week_1/Hello_world.java
@@ -0,0 +1,7 @@
+package week_1;
+
+public class Hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello world!\n" + "(And all the people of the world)");
+    }
+}
