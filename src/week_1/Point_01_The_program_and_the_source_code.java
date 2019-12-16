760ada76fca9479f8a2cc2f53ee7a04ce08a994b
orloveste <orlovevm@yahoo.com>
2019-12-16 09:12:24 +0200

    2. Main program body The body for a program named "Example" is as follows: public class Example {     public static void main(String[] args) {         // program code     } }
    The program is stored in a text file named after the program with the .java extension. For a program named Example, the file should be named Example.java.
    The execution of the program begins at the part marked with the // program code comment above. During our first week of programming, we will limit ourselves to this part. When we are talking about commands such as printing, we need to write the commands into the program body. For example: System.out.print("Text to be printed");

--- src/week_1/Point_01_The_program_and_the_source_code.java
+++ src/week_1/Point_01_The_program_and_the_source_code.java
@@ -1,6 +1,6 @@
 package week_1;
 
-public class The_program_and_the_source_code {
+public class Point_01_The_program_and_the_source_code {
 public static void main(String[]args){
         System.out.println("exemple");
         System.out.println("First\nSecond\nThird\nFourth \bFith\f");
---
+++ src/week_1/Point_02_Main_program_body.java
@@ -0,0 +1,8 @@
+package week_1;
+
+public class Point_02_Main_program_body {
+    public static void main(String[] args) {
+        System.out.println("Done already");
+    }
+}
+
