4ff31c4abd8442b6e0e3de35e11154704f526710
orloveste <orlovevm@yahoo.com>
2019-12-16 09:04:10 +0200

    1.5 More about printing As we can see from the examples above, there are two commands for printing. System.out.print prints the text without the line break at the end System.out.println prints the text and the line break The printed text can contain both traditional characters and special characters. The most important special character is \n, which stands for a line break. There are also other special characters. https://en.wikipedia.org/wiki/Escape_character

--- src/week_1/The_program_and_the_source_code.java
+++ src/week_1/The_program_and_the_source_code.java
@@ -2,11 +2,7 @@ package week_1;
 
 public class The_program_and_the_source_code {
 public static void main(String[]args){
-//    System.out.print("Hello ");
-
-    /*System.out.print("world");
-    System.out.print("!");*/
-
-    //comment in bouth ways
-        }
+        System.out.println("exemple");
+        System.out.println("First\nSecond\nThird\nFourth \bFith\f");
+        }// '\b' is eating backspace.... :)
 }
