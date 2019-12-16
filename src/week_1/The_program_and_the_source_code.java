904bb4165a0a4f84c4bfd6d5ce89dd5ce908d77a
orloveste <orlovevm@yahoo.com>
2019-12-16 08:52:27 +0200

    1.4.2 Parameters (information passed to commands) The information processed by a command are the parameters of a command. They are passed to the command by placing them between () brackets that follow the command name. For example, the System.out.print command is given the text hello as a parameter as follows: System.out.print("hello"). 1.4.3 Comments Comments are a useful way to make notes in the source code for yourself and others. Everything on a line after two forward slashes // is treated as a comment. 1.4.4 Example of using comments // We will print the text "Hello world" System.out.print("Hello world");
    System.out.print(" and all the people of the world."); // We print more text to the same line.
    
    // System.out.print("this line will not be executed, because it is commented out");
    
    The last line of the example introduces a particularly handy use for comments: you can comment out code instead of completely deleting it if you want to temporarily try out something.

--- src/week_1/The_program_and_the_source_code.java
+++ src/week_1/The_program_and_the_source_code.java
@@ -2,7 +2,11 @@ package week_1;
 
 public class The_program_and_the_source_code {
 public static void main(String[]args){
-    System.out.print("Hello ");System.out.print("world");System.out.print("!");
+//    System.out.print("Hello ");
 
+    /*System.out.print("world");
+    System.out.print("!");*/
+
+    //comment in bouth ways
         }
 }
