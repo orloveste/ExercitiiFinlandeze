1e223ae393d2192f0959d83d5c4db3729cb7e338
orloveste <orlovevm@yahoo.com>
2019-12-16 09:45:20 +0200

    Exercise 3: Spruce

---
+++ src/week_1/Spruce.java
@@ -0,0 +1,20 @@
+package week_1;
+
+public class Spruce {
+    public static void main(String[] args) {
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+        System.out.println("    *");
+        System.out.println("//////////");
+        System.out.println(
+                "    *\n" +
+                "   ***\n" +
+                "  *****\n" +
+                " *******\n" +
+                "*********\n" +
+                "    *");
+    }
+}
