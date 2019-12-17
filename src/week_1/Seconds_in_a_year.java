37b929c6c338860f9e3c3ee7562cc7e9736ec59b
orloveste <orlovevm@yahoo.com>
2019-12-17 10:34:03 +0200

    Exercise 5: Seconds in a year

---
+++ src/week_1/Seconds_in_a_year.java
@@ -0,0 +1,15 @@
+package week_1;
+
+public class Seconds_in_a_year {
+    //Exercise 5: Seconds in a year
+    //Create a program that counts how many seconds there are in a year. You can assume that a year consists of 365
+    // days (therefore the year is not a leap year).
+    public static void main(String[] args) {
+        int daysInYear = 365;
+        int hoursInDay = 24;
+        int minutesInHour = 60;
+        int secondsInHour = 60;
+        int resultSecondsInYear = daysInYear*hoursInDay*minutesInHour*secondsInHour;
+        System.out.println("There are " + resultSecondsInYear + " seconds in a year.");
+    }
+}
