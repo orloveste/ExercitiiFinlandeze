83e4fc88bc853a08c9e9b64a2af9257bb5b98d3c
orloveste <orlovevm@yahoo.com>
2019-12-17 10:44:41 +0200

    Exercise 6: Addition step 1

---
+++ src/week_1/Addition.java
@@ -0,0 +1,17 @@
+package week_1;
+public class Addition {
+    public static void main(String[] args) {
+        int firstNumber = 5;
+        int secondNumber = 4;
+        int additionResult = firstNumber+secondNumber;
+        System.out.println(firstNumber +" + "+ secondNumber+" = "+ additionResult);
+    }
+}
+//    Exercise 6: Addition
+//    Create a program to calculate the sum of two numbers. At the beginning of the program two variables are introduced
+//    and those variables hold the numbers to be summed. Feel free to use other variables if you need to.
+//    For example, if the variables hold numbers 5 and 4, the program should output:
+//            5 + 4 = 9
+//
+//    If the variables hold numbers 73457 and 12888, the program output should be:
+//            73457 + 12888 = 86345
