968a6eb06cd58f5c882b4bdf34914072562f9d0a
orloveste <orlovevm@yahoo.com>
2019-12-18 18:49:49 +0200

    Exercise 13: buleala import librarie - nu merge

--- src/week_1/NHL_statistics_part_1.java
+++ src/week_1/NHL_statistics_part_1.java
@@ -2,17 +2,11 @@ package week_1;
 
 import java.util.Scanner;
 public class NHL_statistics_part_1 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         Scanner reader = new Scanner(System.in);
 
         System.out.println("Top ten by points");
-        NHLStatistics.sortByPoints();
-        NHLStatistics.top(10);
-
-        NHLStatistics.sortByPoints();     // orders the players by points
-        NHLStatistics.sortByGoals();      // orders the players by goals
-        NHLStatistics.sortByAssists();    // orders the players by assists
-        NHLStatistics.sortByPenalties();  // orders the players by penalty minutes
-
+//        nhllib.sortByPoints();
+//        NHLStatistics.top(10);
     }
 }
