30740d1c5892a9b5c4a828501331732b94c71eb9
orloveste <orlovevm@yahoo.com>
2019-12-19 15:27:42 +0200

    Exercise 13: NHL statistics, part 1 Create a program that does the following tasks into the main program body. The tasks must be done in exactly the same order as listed below. Do the tasks in the program body one after another without deleting tasks you have already done. Note: When you first run the program, the execution might take a while because the information is downloaded from the web. Execution should be quick after the first run. The program must do the following:             //Print the top ten players based on goals             //Print the top 25 players based on penalty amounts             //Print the statistics for Sidney Crosby             //Print the statistics for Philadelphia Flyers (abbreviation: PHI). Note in which order the players are printed in and why that might be!             //Print the players in Anaheim Ducks (abbreviation: ANA) ordered by points             //After you have successfully submitted the exercise, you can play with the code as you wish!

--- src/week_1/Ex13_NHL_statistics_part_1.java
+++ src/week_1/Ex13_NHL_statistics_part_1.java
@@ -2,13 +2,33 @@ package week_1;
 
 import nhlstats.NHLStatistics;
 
-import java.util.Scanner;
 public class Ex13_NHL_statistics_part_1 {
-    public static void main(String[] args) throws Exception{
-        Scanner reader = new Scanner(System.in);
-
-        System.out.println("Top ten by points");
-
+    public static void main(String[] args) {
+        System.out.println("Top ten players based on goals");
+        NHLStatistics.sortByGoals();
         NHLStatistics.top(10);
+        System.out.println("\nTop 25 players based on penalty amounts");
+        NHLStatistics.sortByPenalties();
+        NHLStatistics.top(25);
+        System.out.println("\nStatistics for Sidney Crosby");
+        NHLStatistics.searchByPlayer("Sidney Crosby");
+        System.out.println("\nStatistics for Philadelphia Flyers");
+        NHLStatistics.teamStatistics("PHI");
+        System.out.println("\nPlayers in Anaheim Ducks (abbreviation: ANA) ordered by points");
+        NHLStatistics.sortByPoints();
+        NHLStatistics.teamStatistics("ANA");
     }
 }
+//Exercise 13: NHL statistics, part 1
+// Create a program that does the following tasks into the main program body. The tasks must be done in exactly the same
+// order as listed below. Do the tasks in the program body one after another without deleting tasks you have already done.
+//Note: When you first run the program, the execution might take a while because the information is downloaded from the
+// web. Execution should be quick after the first run.
+//The program must do the following:
+//Print the top ten players based on goals
+//Print the top 25 players based on penalty amounts
+//Print the statistics for Sidney Crosby
+//Print the statistics for Philadelphia Flyers (abbreviation: PHI). Note in which order the players are printed in and
+// why that might be!
+//Print the players in Anaheim Ducks (abbreviation: ANA) ordered by points
+//After you have successfully submitted the exercise, you can play with the code as you wish!
--- src/week_1/Ex5_Seconds_in_a_year.java
+++ src/week_1/Ex5_Seconds_in_a_year.java
@@ -1,9 +1,6 @@
 package week_1;
 
 public class Ex5_Seconds_in_a_year {
-    //Exercise 5: Seconds in a year
-    //Create a program that counts how many seconds there are in a year. You can assume that a year consists of 365
-    // days (therefore the year is not a leap year).
     public static void main(String[] args) {
         int daysInYear = 365;
         int hoursInDay = 24;
@@ -13,3 +10,6 @@ public class Ex5_Seconds_in_a_year {
         System.out.println("There are " + resultSecondsInYear + " seconds in a year.");
     }
 }
+//Exercise 5: Seconds in a year
+//Create a program that counts how many seconds there are in a year. You can assume that a year consists of 365
+// days (therefore the year is not a leap year).
