6ef09df41ecf1f7fd44b90cc0d56e5468381071d
orloveste <orlovevm@yahoo.com>
2020-01-07 13:45:00 +0200

    Exercise 27: From one to a hundred Create a program that prints the integers (whole numbers) from 1 to 100. The program output should be: 1 2 3 (many rows of numbers here) 98 99 100

---
+++ src/week_2/Ex27_From_one_to_aHundred.java
@@ -0,0 +1,22 @@
+package week_2;
+
+public class Ex27_From_one_to_aHundred {
+    public static void main(String[] args) {
+        int number = 1;
+
+        while (number < 101) {
+            System.out.println(number);
+            number++;
+        }
+    }
+}
+//Exercise 27: From one to a hundred
+//Create a program that prints the integers (whole numbers) from 1 to 100.
+//The program output should be:
+//1
+//2
+//3
+//(many rows of numbers here)
+//98
+//99
+//100
