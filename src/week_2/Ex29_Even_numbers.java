20794b35df1a0f2b2e95f4913bdeba548fcba99d
orloveste <orlovevm@yahoo.com>
2020-01-07 14:05:38 +0200

    Exercise 29: Even numbers Create a program that prints all even numbers between 2 and 100. 2 4 6 (many rows of numbers here) 96 98 100

---
+++ src/week_2/Ex29_Even_numbers.java
@@ -0,0 +1,20 @@
+package week_2;
+
+public class Ex29_Even_numbers {
+    public static void main(String[] args) {
+        int number = 2;
+        while (number < 101){
+            System.out.println(number);
+            number=number+2;
+        }
+    }
+}
+//Exercise 29: Even numbers
+//Create a program that prints all even numbers between 2 and 100.
+//2
+//4
+//6
+//(many rows of numbers here)
+//96
+//98
+//100
