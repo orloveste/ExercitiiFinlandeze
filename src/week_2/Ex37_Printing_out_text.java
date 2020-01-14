3269ba83777a676c9edcc6a0dad0ec7620b251b1
orloveste <orlovevm@yahoo.com>
2020-01-14 09:32:30 +0200

    Exercise 37: Printing out text Create a method printText that prints the following string of characters: "In the beginning there were the swamp, the hoe and Java." and a line break. public static void main(String[] args) {     printText(); }
    public static void printText() {
        // write your code here
    }
    
    The program output:
    In the beginning there were the swamp, the hoe and Java.

---
+++ src/week_2/Ex37_Printing_out_text.java
@@ -0,0 +1,24 @@
+package week_2;
+
+public class Ex37_Printing_out_text {
+    public static void main(String[] args){
+        printText();
+    }
+
+    private static void printText() {
+        System.out.println("In the beginning there were the swamp, the hoe and Java.");
+    }
+}
+//Exercise 37: Printing out text
+//Create a method printText that prints the following string of characters: "In the beginning
+// there were the swamp, the hoe and Java." and a line break.
+//public static void main(String[] args) {
+//    printText();
+//}
+//
+//public static void printText() {
+//    // write your code here
+//}
+//
+//The program output:
+//In the beginning there were the swamp, the hoe and Java.
