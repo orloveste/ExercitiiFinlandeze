b7e2bb57b2a8bf9a539987d642aadf0dc52d065f
orloveste <orlovevm@yahoo.com>
2020-01-17 09:08:22 +0200

    Exercise 41: Guessing a number game In this exercise the following game is created: Guess a number: 73 The number is lesser, guesses made: 1 Guess a number: 22 The number is greater, guesses made: 2 Guess a number: 51 The number is greater, guesses made: 3 Guess a number: 62 The number is greater, guesses made: 4 Guess a number: 68 The number is greater, guesses made: 5 Guess a number: 71 The number is lesser, guesses made: 6 Guess a number: 70 Congratulations, your guess is correct!

---
+++ src/week_2/Ex41_Guessing_a_number_game.java
@@ -0,0 +1,25 @@
+package week_2;
+
+public class Ex41_Guessing_a_number_game {
+    public static void main(String[] args) {
+        System.out.println("Guess a number: ");
+        System.out.println("The number is lesser, guesses made: ");
+        System.out.println("Congratulations, your guess is correct!");
+    }
+}
+//Exercise 41: Guessing a number game
+//In this exercise the following game is created:
+//Guess a number: 73
+//The number is lesser, guesses made: 1
+//Guess a number: 22
+//The number is greater, guesses made: 2
+//Guess a number: 51
+//The number is greater, guesses made: 3
+//Guess a number: 62
+//The number is greater, guesses made: 4
+//Guess a number: 68
+//The number is greater, guesses made: 5
+//Guess a number: 71
+//The number is lesser, guesses made: 6
+//Guess a number: 70
+//Congratulations, your guess is correct!
