3d1a479980e45522f65ca70ebc440e6ef21c618d
orloveste <orlovevm@yahoo.com>
2020-01-29 18:40:17 +0200

    Ex55 on going 4

--- src/week_3/Ex55_A_word_inside_a_word.java
+++ src/week_3/Ex55_A_word_inside_a_word.java
@@ -3,35 +3,27 @@ package week_3;
 import java.util.Scanner;
 
 public class Ex55_A_word_inside_a_word {
+    public static int calculateCharacters(String text){
+        return text.length();
+    }
+
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         System.out.print("Type the first word: ");
         String firstWord = reader.nextLine();
         System.out.print("Type the second word: ");
         String secondWord = reader.nextLine();
-//        int index = firstWord.indexOf(secondWord);
-//        int index = firstWord.indexOf(secondWord.length());
-//        String compareString = String.valueOf(secondWord.compareToIgnoreCase(firstWord));
-//        int finalIndex = secondWord.length();
-//        int finalIndex = firstWord.codePointCount(secondWord);
-//        System.out.println(compareString);
+
+        int index = firstWord.indexOf(secondWord);
+        int lastIndex = firstWord.lastIndexOf(secondWord);
+
         if (firstWord.contains(secondWord)){
-            System.out.println("The word '"+secondWord+"' is found in the word '"+firstWord+"'.");
+            System.out.println("The word '"+firstWord.substring(index,lastIndex)+"' is found in the word '"+firstWord+"'.");
         } else {
             System.out.println("The word '"+secondWord+"' is not found in the word '"+firstWord+"'.");
         }
     }
 }
- /*while (true){
-            System.out.println("The word '"+firstWord.substring(index)+"' is found in the word '"+firstWord+"'.");
-            break;
-        }
-        System.out.println("The word '"+secondWord+"' is not found in the word '"+firstWord+"'.");*///test1
- /*if (true){
-            System.out.println("The word '"+firstWord.substring(index)+"' is found in the word '"+firstWord+"'.");
-        }else {
-            System.out.println("The word '"+secondWord+"' is not found in the word '"+firstWord+"'.");
-        }*///test2
 //Exercise 55: A word inside a word
 //Create a program that asks the user for two words. Then the program tells if the second
 // word
