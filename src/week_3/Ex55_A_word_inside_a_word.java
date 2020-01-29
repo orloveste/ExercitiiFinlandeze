22a37906a3af1906f214595bed89fa0a077c0b90
orloveste <orlovevm@yahoo.com>
2020-01-29 17:05:35 +0200

    Exercise 55: PARTIAL RECOGNIZE FIRST CHAR - A word inside a word Create a program that asks the user for two words. Then the program tells if the second word is included in the first word. Use String method indexOf in your program. Type the first word: glitter Type the second word: litter The word 'litter' is found in the word 'glitter'.
    Type the first word: glitter
    Type the second word: clean
    The word 'clean' is not found in the word 'glitter'.
    
    Note: Make your program outputs (prints) match exactly the example above!

---
+++ src/week_3/Ex55_A_word_inside_a_word.java
@@ -0,0 +1,47 @@
+package week_3;
+
+import java.util.Scanner;
+
+public class Ex55_A_word_inside_a_word {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Type the first word: ");
+        String firstWord = reader.nextLine();
+        System.out.print("Type the second word: ");
+        String secondWord = reader.nextLine();
+        int index = firstWord.indexOf(secondWord);
+//        int index = firstWord.indexOf(secondWord.length());
+//        String compareString = String.valueOf(secondWord.compareToIgnoreCase(firstWord));
+//        int finalIndex = secondWord.length();
+//        int finalIndex = firstWord.codePointCount(secondWord);
+//        System.out.println(compareString);
+        if (index >= 0){
+            System.out.println("The word '"+firstWord.substring(index/*, finalIndex+1*/)+"' is found in the word '"+firstWord+"'.");
+        } else {
+            System.out.println("The word '"+secondWord+"' is not found in the word '"+firstWord+"'.");
+        }
+    }
+}
+ /*while (true){
+            System.out.println("The word '"+firstWord.substring(index)+"' is found in the word '"+firstWord+"'.");
+            break;
+        }
+        System.out.println("The word '"+secondWord+"' is not found in the word '"+firstWord+"'.");*///test1
+ /*if (true){
+            System.out.println("The word '"+firstWord.substring(index)+"' is found in the word '"+firstWord+"'.");
+        }else {
+            System.out.println("The word '"+secondWord+"' is not found in the word '"+firstWord+"'.");
+        }*///test2
+//Exercise 55: A word inside a word
+//Create a program that asks the user for two words. Then the program tells if the second
+// word
+// is included in the first word. Use String method indexOf in your program.
+//Type the first word: glitter
+//Type the second word: litter
+//The word 'litter' is found in the word 'glitter'.
+//
+//Type the first word: glitter
+//Type the second word: clean
+//The word 'clean' is not found in the word 'glitter'.
+//
+//Note: Make your program outputs (prints) match exactly the example above!
