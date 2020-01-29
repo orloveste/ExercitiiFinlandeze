40a4e8c8899a425b65dd431a7ad091cccffc76ae
orloveste <orlovevm@yahoo.com>
2020-01-29 11:11:53 +0200

    15.3 Other methods for strings Methods in the String class also make it possible to search for a specific word in  text. For example, the word "or" can be found in the word "Horse". A method called  indexOf() searches for the word given as a parameter in a string. If the word is  found, it returns the starting index (location), remember that the numbering starts  from 0 of the word. If the word is not found, the method returns the value -1. String word = "aesthetically";
    int index = word.indexOf("tic"); // index value will be 6
    System.out.println(word.substring(index)); //prints "tically"
    
    index = word.indexOf("ally"); //index value will be 9
    System.out.println(word.substring(index)); //prints "ally"
    
    index = word.indexOf("book"); // string "aesthetically" does not include "book"
    System.out.println(index); //prints -1
    System.out.println(word.substring(index)); //error!

--- src/week_3/Week3_learn.java
+++ src/week_3/Week3_learn.java
@@ -2,15 +2,33 @@ package week_3;
 
 public class Week3_learn {
     public static void main(String[] args) {
-        String book = "Mary Poppins - and";
-        String endPart = book.substring(4);
+        String word = "aesthetically";
 
-        System.out.println("Harry"+endPart);
+        int index = word.indexOf("tic");
+        System.out.println(word.substring(index));
+
+        index = word.indexOf("ally");
+        System.out.println(word.substring(index));
+
+        index = word.indexOf("book");
+        System.out.println(index);
+        System.out.println(word.substring(index));
     }
 }
-//15.2 Other methods for strings
-//We can store the return value in a variable, because the return value of the substring
-// method is of type String.
-//String book = "Mary Poppins";
-//String endpart = book.substring(5);
-//System.out.println("Harry " + endpart); // prints "Harry Poppins"
+//15.3 Other methods for strings
+//Methods in the String class also make it possible to search for a specific word in
+// text. For example, the word "or" can be found in the word "Horse". A method called
+// indexOf() searches for the word given as a parameter in a string. If the word is
+// found, it returns the starting index (location), remember that the numbering starts
+// from 0 of the word. If the word is not found, the method returns the value -1.
+//String word = "aesthetically";
+//
+//int index = word.indexOf("tic"); // index value will be 6
+//System.out.println(word.substring(index)); //prints "tically"
+//
+//index = word.indexOf("ally"); //index value will be 9
+//System.out.println(word.substring(index)); //prints "ally"
+//
+//index = word.indexOf("book"); // string "aesthetically" does not include "book"
+//System.out.println(index); //prints -1
+//System.out.println(word.substring(index)); //error!
