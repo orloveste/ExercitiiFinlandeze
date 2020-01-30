314a8d2dde69b4168278facbd342fdf2011b8f33
orloveste <orlovevm@yahoo.com>
2020-01-30 11:14:16 +0200

    ex55 test - find secondWord in firstWord, but is showing rest of firstWord

--- src/week_3/Ex55_A_word_inside_a_word.java
+++ src/week_3/Ex55_A_word_inside_a_word.java
@@ -1,5 +1,8 @@
 package week_3;
 
+import com.sun.xml.internal.ws.policy.PolicyException;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ex55_A_word_inside_a_word {
@@ -7,29 +10,33 @@ public class Ex55_A_word_inside_a_word {
         return text.length();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PolicyException {
         Scanner reader = new Scanner(System.in);
         System.out.print("Type the first word: ");
         String firstWord = reader.nextLine();
         System.out.print("Type the second word: ");
         String secondWord = reader.nextLine();
-        for (int i = 0; i<firstWord.length();++i){
-            if (firstWord.substring(i).equals(secondWord)){
-                System.out.println("The word '"+secondWord+"' is found in the word '"+firstWord+"'.");
-            }
-        }
 
 //        int index = firstWord.indexOf(secondWord);
-//        int lastIndex = calculateCharacters(secondWord)+1;
-//
+//        int lastIndex = calculateCharacters(secondWord)-secondWord.length();
+
+//        Pattern pattern = Pattern.compile("(?<!\\S)"+ secondWord+"(?!\\S)");
+//        boolean matcher = Pattern.matches(firstWord, secondWord);
+//        AssertionValidationProcessor.getInstance();
+
 //        int lastIndex = firstWord.subSequence(index, secondWord.length());
 //        int lastIndex = firstWord.lastIndexOf(secondWord.length());
 
-//        if (firstWord.contains(secondWord)){
-//            System.out.println("The word '"+firstWord.substring(index, lastIndex)+"' is found in the word '"+firstWord+"'.");
-//        } else {
-//            System.out.println("The word '"+secondWord+"' is not found in the word '"+firstWord+"'.");
-//        }
+        if (firstWord.contains(secondWord)){
+            System.out.println("The word '"+ Arrays.toString(firstWord.split(secondWord, secondWord.length())) +"' is found in the word '"+firstWord+"'.");
+        } else {
+            System.out.println("The word '"+secondWord+"' is not found in the word '"+firstWord+"'.");
+        }
+        /*if (firstWord.contains(secondWord)){
+            System.out.println("The word '"+firstWord.substring(index)+"' is found in the word '"+firstWord+"'.");
+        } else {
+            System.out.println("The word '"+secondWord+"' is not found in the word '"+firstWord+"'.");
+        }*/
     }
 }
 //Exercise 55: A word inside a word
@@ -45,3 +52,9 @@ public class Ex55_A_word_inside_a_word {
 //The word 'clean' is not found in the word 'glitter'.
 //
 //Note: Make your program outputs (prints) match exactly the example above!
+
+/*for (int i = 0; i<firstWord.length();++i){
+            if (firstWord.substring(i).equals(secondWord)){
+                System.out.println("The word '"+secondWord+"' is found in the word '"+firstWord+"'.");
+            }
+        }*///test for
