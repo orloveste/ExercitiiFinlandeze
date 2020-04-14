4c75cfe2008390ff22565b86bed479db08a0e926
orloveste <orlovevm@yahoo.com>
2020-04-14 13:14:21 +0300

    Exercise 57: Words - toDo
    Create a program that asks the user to input words until the user types in an empty String. Then the program prints the words the user gave. Try the for repetition sentence here. Use an ArrayList structure in your program. ArrayList is defined like this:
    ArrayList<String> words = new ArrayList<String>();
    
    Type a word: Mozart
    Type a word: Schubert
    Type a word: Bach
    Type a word: Sibelius
    Type a word: Liszt
    Type a word:
    You typed the following words:
    Mozart
    Schubert
    Bach
    Sibelius
    Liszt
    
    Note: an empty String can be detected this way:
    String word = reader.nextLine();
    
    if ( word.isEmpty() ) {  // could also be: word.equals("")
       // word was empty, meaning that the user only pressed enter
    }

---
+++ src/week_3/Ex57_Words.java
@@ -0,0 +1,33 @@
+package week_3;
+
+public class Ex57_Words {
+    public static void main(String[] args) {
+
+    }
+}
+//    Create a program that asks the user to input words until the user types
+//    in an empty String. Then the program prints the words the user gave. Try
+//    the for repetition sentence here. Use an ArrayList structure in your
+//    program. ArrayList is defined like this:
+//        ArrayList<String> words = new ArrayList<String>();
+//
+//        Type a word: Mozart
+//        Type a word: Schubert
+//        Type a word: Bach
+//        Type a word: Sibelius
+//        Type a word: Liszt
+//        Type a word:
+//        You typed the following words:
+//        Mozart
+//        Schubert
+//        Bach
+//        Sibelius
+//        Liszt
+//
+//        Note: an empty String can be detected this way:
+//        String word = reader.nextLine();
+//
+//        if ( word.isEmpty() ) {  // could also be: word.equals("")
+//        // word was empty, meaning that the user only pressed enter
+//        }
+
